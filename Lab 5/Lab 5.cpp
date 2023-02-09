acda87730b6324022e603de0b62dc7aeae221761
emrichj <emrichj@X-KNOY306PC17>
2023-02-09 14:27:47 -0500

    Add project files.

---
+++ Lab 5/Lab 5.cpp
@@ -0,0 +1,40 @@
+// Lab 5.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+
+vector<int> intVector({ 'V','F','X','B','L','I','T','Z','J','R','P','H','D','K','N','O','W','S','G','U','Y','Q','M','A','C','E' });
+
+
+if (65 < char < 90); //capital letter
+	return (intVector [char - 65]);
+else if (97 < char < 122); //lower case
+	set char 
+ 
+
+
+int main()
+{
+	std::string text;
+
+	cout << "enter text" << endl;
+	getline(cin, text);
+	cout << text << endl;
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
